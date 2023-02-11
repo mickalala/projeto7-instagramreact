8e286671df2f2a22beed2270a95622e46d48df26
micka <mickalarissa01@gmail.com>
2023-02-11 17:07:52 -0300

    antes de começar props

---
+++ Driven/Módulo 2/projeto7-instagramreact/src/Componentes/App.js
@@ -0,0 +1,12 @@
+import Navbar from "./Navbar"
+import Corpo from "./Corpo"
+
+export default function App() {
+    return (
+        <div>
+            <Navbar />
+
+            <Corpo />
+        </div>
+    )
+}
